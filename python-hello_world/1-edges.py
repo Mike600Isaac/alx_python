148daa5e8d3e895741bb21329da7850eae5ed6d4
Mike603Isaac <isaacmicheal603@gmail.com>
2023-08-04 15:16:59 -0700

    This is my pythin file

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -2,5 +2,5 @@
 word = "Holberton"
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
 print("First 3 letters: {}".format(word[:3]))
-print("Last 2 letters: {0}{1}".format(word[len(word)-1], word[len(word)-2]))
+print("Last 2 letters: {1}{0}".format(word[len(word)-1], word[len(word)-2]))
 print("Middle word: {}".format(word[1:8]))
