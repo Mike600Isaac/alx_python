dde906c65135f975c8fed286b39f952643d9c022
Mike603Isaac <isaacmicheal603@gmail.com>
2023-08-04 14:11:07 -0700

    This is my python file

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 word = "Holberton"
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print("word_first_3: {0}".format(word[:3]))
-print("word_last_2: {0}".format(word[7:9]))
-print("middle_word: {0}{1}".format(word[1:8],word[-1]))
+print("First 3 letters: {0}".format(word[:3]))
+print("Last 2 letters: {0}".format(word[7:9]))
+print("Middle word: {0}{1}".format(word[1:8],word[-1]))
