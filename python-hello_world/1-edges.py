dee8cd021b10041f716aaedcd950f3f3a764644e
Mike603Isaac <isaacmicheal603@gmail.com>
2023-08-04 15:46:15 -0700

    This is my python file

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -4,4 +4,5 @@ word = "Holberton"
 print("First 3 letters: {}".format(word[:3]))
 print("Last 2 letters: {1}{0}".format(word[len(word)-1], word[len(word)-2]))
 word = word[1:]
-print("Middle word: {}".format(word[:-1]))
+word = word[:-1]
+print("Middle word: {}".format(word))
