79d6164f4ed179c9df0c71282ce6e0fda702243d
Micheal Isaac <isaacmicheal600@gmail.com>
2023-08-18 06:16:17 -0700

    This is my python file

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -14,16 +14,18 @@
 
 #!/usr/bin/python3
 import random
+
 number = random.randint(-10000, 10000)
-last = number % 10
-s_num = "Last digit of"
-fi_ve = "and is greater than 5"
-six_zero = "and is less than 6 and not 0"
-if (last > 5):
-    print(f"{s_num} {number} is {last} {fi_ve}")
-elif (last == 0):
-    print(s_num + " {} is {} and is 0".format(str(number), str(last)))
-elif (last < 6 and last != 0):
-    last = abs(number) % 10
-    neg_last = -last
-    print(f"{s_num} {number} is {neg_last} {six_zero}")
+
+# Get the last digit of the number
+last_digit = abs(number) % 10  # We use abs to ensure that negative numbers also give positive last digits
+
+print("Last digit of", number, "is", last_digit, end=" ")
+
+if last_digit > 5:
+    print("and is greater than 5")
+elif last_digit == 0:
+    print("and is 0")
+else:  # last_digit is less than 6 and not 0
+    print("and is less than 6 and not 0")
+
