d31bbcf82b55b6c10f6a4300a420b5e566e23134
Micheal Isaac <isaacmicheal600@gmail.com>
2023-08-22 06:45:25 -0700

    A for loop to print range of numbers

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,7 +1,4 @@
 
 #!/usr/bin/python3
-for i in range(100):
-    if i == 99:
-        print("{:d}".format(i))
-    else:
-        print("{:02d}, ".format(i), end="")
+for num in range(99):
+    print(("{:2d} = 0x{:x}").format(num, num))
