e0cc025a069ea3f56de765873d6c0c448048db58
Micheal Isaac <isaacmicheal600@gmail.com>
2023-08-22 06:34:28 -0700

    A program to print the last digit of a number

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -31,11 +31,29 @@
 
 #!/usr/bin/python3
 #!/usr/bin/python3
+# import random
+# number = random.randint(-10000, 10000)
+# if number > 0:
+#     print("{} is positive".format(number))
+# elif number == 0:
+#     print("{} is zero".format(number))
+# else:
+#     print("{} is negative".format(number))
+
+
+#!/usr/bin/python3
 import random
+
 number = random.randint(-10000, 10000)
-if number > 0:
-    print("{} is positive".format(number))
-elif number == 0:
-    print("{} is zero".format(number))
+
+Last_digit = abs(number) % 10
+print("Last digit of", number, "is", end=" ")
+if Last_digit > 5 and number > 0:
+    print(Last_digit, "and is greater than 5", end="\n")
+elif Last_digit == 0:
+    print(Last_digit, "and is 0", end="\n")
 else:
-    print("{} is negative".format(number))
+    if number > 0:
+        print(Last_digit, "and is less than 6 and not 0", end="\n")
+    else:
+        print(-Last_digit, "and is less than 6 and not 0", end="\n")
