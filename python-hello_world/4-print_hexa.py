1d74a897ea16c0b358ded71c2ec319537c2fe776
Micheal Isaac <isaacmicheal600@gmail.com>
2023-08-22 07:41:26 -0700

    This is my python file

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,3 +1,7 @@
 
-for num in range(99):
-    print(("{:2d} = 0x{:x}").format(num, num))
+# for num in range(99):
+#     print(("{:2d} = 0x{:x}").format(num, num))
+
+#!/usr/bin/python3
+for i in range(99):
+    print("{:d} = 0x{:x}".format(i, i))
