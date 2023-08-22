58076d0dba0b6cb979b1379c70ca1da05ee71ad2
Micheal Isaac <isaacmicheal600@gmail.com>
2023-08-22 06:43:14 -0700

    A program to print dec to hexa numbers

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -8,7 +8,6 @@ elif number == 0:
     print("{} is zero".format(number))
 else:
     print("{} is negative".format(number))
-# task 3
-#!/usr/bin/python3
+
 for i in range(99):
     print("{:d} = 0x{:x}".format(i, i))
