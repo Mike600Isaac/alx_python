a6b6565f5e9e2f6113a62a52f8dbb2d03f96e72e
Micheal Isaac <isaacmicheal600@gmail.com>
2023-09-11 04:49:20 -0700

    function to raise power of two numbers

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,9 +1,22 @@
-def pow(a, b):
-  return a ** b
+# def pow(a, b):
+#   return a ** b
 
-pow = __import__('1-power').pow
-print(pow(2, 2))
-print(pow(98, 2))
-print(pow(98, 0))
-print(pow(100, -2))
-print(pow(-4, 5))
+# pow = __import__('1-power').pow
+# print(pow(2, 2))
+# print(pow(98, 2))
+# print(pow(98, 0))
+# print(pow(100, -2))
+# print(pow(-4, 5))
+
+#!/usr/bin/env python3
+def main():
+    (pow(2, 2))
+    (pow(98, 2))
+    (pow(98, 0))
+    (pow(100, -2))
+    (pow(-4, 5))
+
+def pow(a,b):
+    return a**b
+
+main()
