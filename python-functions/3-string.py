e7b09b29bae9eaa1d1cfa10f180f3c34f9fec855
Micheal Isaac <isaacmicheal600@gmail.com>
2023-09-11 08:43:58 -0700

    function to reverse string

--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -1,11 +1,8 @@
-def main():
-  print(reverse_string("Hello"))         # Output: "olleH"
-  print(reverse_string(""))              # Output: ""
-  print(reverse_string("madam"))         # Output: "madam"
-  print(reverse_string("Hello, World!")) # Output: "!dlroW ,olleH"
-
-
-def reverse_string(string, end=''):
+def reverse_string(string):
     reversed = string[::-1]
-    return reversed
-main()
+    return (reversed)
+
+print(reverse_string("Hello"))         # Output: "olleH"
+print(reverse_string(""))              # Output: ""
+print(reverse_string("madam"))         # Output: "madam"
+print(reverse_string("Hello, World!")) # Output: "!dlroW ,olleH"
