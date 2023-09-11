860fd9090895a5504989559a472bfdb273873519
Micheal Isaac <isaacmicheal600@gmail.com>
2023-09-11 05:06:45 -0700

    python function to add two integers

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -20,13 +20,11 @@
 
 
 
-
-
 # Function to add two integers
 def add(a, b, end=''):
     while b != 0:
         carry = a & b
-        a = a + b
+        a = a ^ b
         b = carry << 1
 
     return a
