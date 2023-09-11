a175ab293fe6bcac9fa4e823a77a9536bd83ce0d
Micheal Isaac <isaacmicheal600@gmail.com>
2023-09-11 07:52:33 -0700

    function for fibanacci series

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,12 +1,29 @@
+# def fibonacci_sequence(n):
+#   if n <= 0:
+#     return []
+#   elif n == 1:
+#     return [1]
+#   elif n == 2:
+#     return [0,1]
+#   fib_sequence = [0,1]
+#   while (len(fib_sequence) < n):
+#     next_numb = fib_sequence(-1) + fib_sequence(-2)
+#     fibonacci_sequence(next_numb)
+#   return fib_sequence
+
+
+#!/usr/bin/env python3
 def fibonacci_sequence(n):
-  if n <= 0:
-    return []
-  elif n == 1:
-    return [1]
-  elif n == 2:
-    return [0,1]
-  fib_sequence = [0,1]
-  while (len(fib_sequence) < n):
-    next_numb = fib_sequence(-1) + fib_sequence(-2)
-    fibonacci_sequence(next_numb)
-  return fib_sequence
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+
+    fib_sequence = [0, 1]
+    while len(fib_sequence) < n:
+        next_num = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_num)
+
+    return fib_sequence
