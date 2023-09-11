d936a47760813ff3759892111aca2b67f7d46ca9
Micheal Isaac <isaacmicheal600@gmail.com>
2023-09-11 06:22:19 -0700

    function to convert fehrenheit to celsius

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,26 +1,27 @@
-# def convert_to_celsius(F):
-#   celsius = (F - 32) * 5/9
-#   return celsius
+def convert_to_celsius(F):
+  celsius = (F - 32) * 5/9
+  return celsius
 
-# print(convert_to_celsius(100))
-# print(convert_to_celsius(-40))
-# print(convert_to_celsius(-459.67))
-# print(convert_to_celsius(32))
-
+def main():
+  print(convert_to_celsius(100))
+  print(convert_to_celsius(-40))
+  print(convert_to_celsius(-459.67))
+  print(convert_to_celsius(32))
+main()
 # convert_to_celsius = __import__('2-temperature').convert_to_celsius
 
 
 
-def convert_to_celsius(fahrenheit, end = ''):
-    celsius = (fahrenheit - 32) * 5 / 9
-    return celsius
+# def convert_to_celsius(fahrenheit, end = ''):
+#     celsius = (fahrenheit - 32) * 5 / 9
+#     return celsius
 
-print = (convert_to_celsius)
+# print = (convert_to_celsius)
 
-print(convert_to_celsius(100))
+# print(convert_to_celsius(100))
 
-print(convert_to_celsius(-40))
+# print(convert_to_celsius(-40))
 
-print(convert_to_celsius(-459.67))
+# print(convert_to_celsius(-459.67))
 
-print(convert_to_celsius(32))
+# print(convert_to_celsius(32))
