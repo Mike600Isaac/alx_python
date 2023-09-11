afb79881cb81e20f9fe991c779465bf41c39ee6c
Micheal Isaac <isaacmicheal600@gmail.com>
2023-09-11 04:25:13 -0700

    Function to add two integers

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,14 +1,20 @@
 def add(a,b):
   return a + b
-# print(add(1,2))
-# print(add(98,0))
-# print(add(100,-2))
 
-add = __import__('0-sum').add
+print(add(1,2))
+print(add(98,0))
+print(add(100,-2))
 
-print(add(1, 2))
-print(add(98, 0))
-print(add(100, -2))
+# add = __import__('0-sum').add
+
+# print(add(1, 2))
+# print(add(98, 0))
+# print(add(100, -2))
+
+
+
+
+
 
 # Function to add two integers
 # def add(a, b, end=''):
