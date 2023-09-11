51a571f8096fdc9923adae5300e1469e316de765
Micheal Isaac <isaacmicheal600@gmail.com>
2023-09-11 08:30:01 -0700

    function to convert fah to celsius

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,10 +1,17 @@
-def convert_to_celsius(fahrenheit):
+
+#!/usr/bin/env python3
+#!/usr/bin/env python3
+
+def convert_to_celsius(fahrenheit, end = ''):
     celsius = (fahrenheit - 32) * 5 / 9
     return celsius
 
 print = (convert_to_celsius)
 
 print(convert_to_celsius(100))
+
 print(convert_to_celsius(-40))
+
 print(convert_to_celsius(-459.67))
+
 print(convert_to_celsius(32))
