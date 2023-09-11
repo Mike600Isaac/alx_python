5c1100f370ed9c5d18dfd9a35da201140bf1b897
Micheal Isaac <isaacmicheal600@gmail.com>
2023-09-11 04:22:28 -0700

    Function to add two integers

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,21 +1,27 @@
-# def add(a,b):
-#   return a + b
+def add(a,b):
+  return a + b
 # print(add(1,2))
 # print(add(98,0))
 # print(add(100,-2))
 
+add = __import__('0-sum').add
+
+print(add(1, 2))
+print(add(98, 0))
+print(add(100, -2))
+
 # Function to add two integers
-def add(a, b, end=''):
-    while b != 0:
-        carry = a & b
-        a = a ^ b
-        b = carry << 1
+# def add(a, b, end=''):
+#     while b != 0:
+#         carry = a & b
+#         a = a ^ b
+#         b = carry << 1
 
-    return a
+#     return a
 
-# Main code
-if __name__ == "__main__":
-    print(add(1, 2))
-    print(add(98, 0))
-    print(add(100, -2))
-    print(add(100, -2))
+# # Main code
+# if __name__ == "__main__":
+#     print(add(1, 2))
+#     print(add(98, 0))
+    # print(add(100, -2))
+    # print(add(100, -2))
