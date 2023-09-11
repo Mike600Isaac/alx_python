8682c3269d403af9f737f237139ce7064bcfde73
Micheal Isaac <isaacmicheal600@gmail.com>
2023-09-11 06:56:36 -0700

    function to reverse string

--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-def reverse_string(string):
-  reversed = string[::-1]
-  return (reversed)
+# def reverse_string(string):
+#   reversed = string[::-1]
+#   return (reversed)
 
-# reverse_string = __import__('3-string').reverse_string
-print(reverse_string("Hello"))
-print(reverse_string(""))
-print(reverse_string("madam"))
-print(reverse_string("Hello, World!"))
+# # reverse_string = __import__('3-string').reverse_string
+# print(reverse_string("Hello"))
+# print(reverse_string(""))
+# print(reverse_string("madam"))
+# print(reverse_string("Hello, World!"))
 
 
 
