45d39abd3f76fa29e239f24c14b422bceea76f9d
Micheal Isaac <isaacmicheal600@gmail.com>
2023-09-11 05:37:19 -0700

    function to convert from fahrenheit to celsius

---
+++ python-functions/2-temperature.py
@@ -0,0 +1,8 @@
+def convert_to_celsius(F):
+  return (F - 32) * (5/9)
+
+convert_to_celsius = __import__('2-temperature').convert_to_celsius
+print(convert_to_celsius(100))
+print(convert_to_celsius(-40))
+print(convert_to_celsius(-459.67))
+print(convert_to_celsius(32))
