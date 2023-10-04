9656793dd7c65f7043830379ddf4b5705d23f96c
Micheal Isaac <isaacmicheal600@gmail.com>
2023-10-04 01:19:43 -0700

    function returns set of common elements in two set

---
+++ python-more_data_structures/1-common_elements.py
@@ -0,0 +1,13 @@
+def common_elements(set_1, set_2):
+    common_set = set()
+    for element in set_1:
+        if element in set_2:
+            common_set.add(element)
+    return common_set
+
+# Test the function
+if __name__ == "__main__":
+    set_1 = { "Python", "C", "Javascript" }
+    set_2 = { "Bash", "C", "Ruby", "Perl" }
+    c_set = common_elements(set_1, set_2)
+    print(sorted(list(c_set)))
