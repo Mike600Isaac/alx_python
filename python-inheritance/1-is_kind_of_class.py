89bda369112b274a5bbe03d3ffd0ce36d29ee3e1
Micheal Isaac <isaacmicheal600@gmail.com>
2023-11-02 04:38:31 -0700

    intheritance task 1

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -1,32 +1,11 @@
-"""
-    Check if the given object is exactly an instance of the specified class.
+# Write a function that returns True if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class ; otherwise False.
 
-    Parameters:
-        obj: Any Python object.
-        a_class: A Python class or class name to compare the type of the object against.
+is_same_class = __import__('0-is_same_class').is_same_class
 
-    Returns:
-        bool: True if the object is an instance of the specified class; otherwise, False.
-
-    Example:
-        >>> class MyClass:
-        ...     pass
-        ...
-        >>> obj1 = MyClass()
-        >>> obj2 = "Hello"
-        >>> obj3 = 42
-        >>>
-        >>> is_same_class(obj1, MyClass)
-        True
-        >>> is_same_class(obj2, str)
-        True
-        >>> is_same_class(obj3, list)
-        False
-    """
-
-
-def is_same_class(obj, a_class):
-    """
-    Check if the given object is exactly an instance of the specified class.
-    """
-    return type(obj) is a_class
+a = 1
+if is_same_class(a, int):
+    print("{} is an instance of the class {}".format(a, int.__name__))
+if is_same_class(a, float):
+    print("{} is an instance of the class {}".format(a, float.__name__))
+if is_same_class(a, object):
+    print("{} is an instance of the class {}".format(a, object.__name__))
