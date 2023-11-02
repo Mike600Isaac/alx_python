1799fb2697d5fd06f75675e5c54a80904edf805b
Micheal Isaac <isaacmicheal600@gmail.com>
2023-11-02 04:12:35 -0700

    Write a class Square that defines a square

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,39 +1,71 @@
+"""
+This class represent a square with a given size
+
+Attribute:
+    _size(int): The size of the square(private attribute)
+"""
+
+
 class Square:
+    """
+    This class represent a Square with a given size
+    """
+
     def __init__(self, size=0):
+        """
+        initialize a square object with a optional size
+
+        Args:
+            size(int): The size of the Square(default is 0)
+        Raises:
+            TypeError: if size is not an integer
+            ValueError: if the size is less than 0
+        """
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-my_square_1 = Square(3)
-print(type(my_square_1))
-print(my_square_1.__dict__)
+    def area(self):
+        """
+        Calculate the area of the Square
+        Return:
+            int: The area of square
+        """
+        return self.__size ** 2
 
-my_square_2 = Square()
-print(type(my_square_2))
-print(my_square_2.__dict__)
+    def perimeter(self):
+        """
+        calculate the perimeter of the square
+        returns:
+            int: the perimeter of the square
+        """
+        return 4 * self.__size
 
-try:
-    print(my_square_1.size)
-except Exception as e:
-    print(e)
+    def get_size(self):
+        """
+        Gets size of square
 
-try:
-    print(my_square_1.__size)
-except Exception as e:
-    print(e)
+        Return:
+        int: The size of the square
+        """
+        return self.__size
 
-try:
-    my_square_3 = Square("3")
-    print(type(my_square_3))
-    print(my_square_3.__dict__)
-except Exception as e:
-    print(e)
+    def set_size(self, new_size):
+        """
+        sets the size of the square to a new value
 
-try:
-    my_square_4 = Square(-89)
-    print(type(my_square_4))
-    print(my_square_4.__dict__)
-except Exception as e:
-    print(e)
+        Args:
+            new_size (int): the new size to set
+
+        Raises:
+            TypeError: if the new size is not an integer
+            ValueError: if the new size < 0
+        """
+        if not isinstance(new_size, int):
+            raise TypeError("size must be an integer")
+        if new_size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = new_size
